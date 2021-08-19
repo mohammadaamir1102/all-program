582b48b36f47e80b103e95aecc33506342107b9e
mohammadaamir1102 <er.aamir1102@gmail.com>
2021-08-19 21:05:38 +0530

    first

---
+++ spring-boot-full-tutorial/src/main/java/com/full/tut/SpringBootFullTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.full.tut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFullTutorialApplication.class, args);
+	}
+
+}
---
+++ spring-boot-full-tutorial/src/test/java/com/full/tut/SpringBootFullTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.full.tut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFullTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
