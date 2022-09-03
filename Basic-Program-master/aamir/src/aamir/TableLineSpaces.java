6e4e7b41fe35a275dafbdcd3e8778815ad2990b4
mohammadaamir1102 <er.aamir1102@gmail.com>
2022-09-03 21:43:42 +0530

    add note stream related

--- Basic-Program-master/aamir/src/aamir/TableLineSpaces.java
+++ Basic-Program-master/aamir/src/aamir/TableLineSpaces.java
@@ -3,17 +3,17 @@ package aamir;
 public class TableLineSpaces {
 
 	public static void main(String[] args) {
-		for(int i=1;i<=10;i++) {
-			for(int a=1;a<=10;a++) {
-				System.out.print(i*a);
+		for (int i = 1; i <= 10; i++) {
+			for (int a = 2; a <= 10; a++) {
+				System.out.print(i * a);
 				System.out.print("\t");
-				if(a==10) {
+				if (a == 10) {
 					System.out.println("\n");
-				
+
+				}
 			}
+
 		}
 
 	}
-
-	}
-	}
+}
